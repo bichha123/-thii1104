ce712a9acb1534eba7569de2fc12c8db57f58c2f
bichha123 <103437331+bichha123@users.noreply.github.com>
2022-04-11 22:12:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ const CONFIG = {
     introDesc: `Hôm nay là 11/04/2022 nè
     Là sinh nhật của một bạn rất cute 😚`,
     btnIntro: '^^Chúc mừng sinh nhật^^',
-    title: 'Tuổi mới luôn xinh đẹp, tươi vui, luôn iu bạn Hà 😘 🥰',
-    desc: 'Chúc bạn thi đậu được nguyện vọng 1 nè, luôn thành công trong học tập 💕',
+    title: 'Tuổi mới luôn xinh đẹp, tươi vui, luôn iu bạn Hà 😘 🥰. Chúc bạn thi đậu được nguyện vọng 1 nè, luôn thành công trong học tập 💕 ',
     desc: 'Vài bữa rảnh đi ăn với Hà và Ly nhó',
     btnYes: 'Có ',
     btnNo: 'Không nha :3',
